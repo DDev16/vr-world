3644e19ec641ef2c7ed1d1c441fa504beb8c53f3
DDev16 <dillondevera12@gmail.com>
2023-07-18 02:14:34 -0700

    lets see

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import FrontPage from './component/FrontPage/FrontPage.js';
+import FrontPage from './component/FrontPage/FrontPage.js';
 import SeussWorld from './component/suessworld.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-      {/* <Route path="/" element={<FrontPage />} /> */}
+      <Route path="/" element={<FrontPage />} />
         <Route path="/" element={<SeussWorld />} />
       </Routes>
     </Router>
